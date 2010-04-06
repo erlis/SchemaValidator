7afb57c4c2c5ee281b343f38e053d4d28112b41a
Erlis Vidal <erlis.vidal@gmail.com>
2010-04-06 16:00:26 -0400

    ADDED: Creating the SchemaTests.cs

---
+++ SchemaValidator.Tests/DBSchemaTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SchemaValidator.Tests
+{
+    [TestFixture]
+    class DBSchemaTests
+    {
+
+        [Test]
+        public void RequireTable_should_return_clase_Table()
+        {
+            // Arrange
+            DBSchema dbSchema = new DBSchema();
+
+            // Act
+            Table result = dbSchema.RequireTable("TableName");
+
+            // Assert
+            Assert.That(result.Name, Is.EqualTo("TableName"));
+        }
+    }
+}
