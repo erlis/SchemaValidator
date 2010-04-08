4c69f6da4d3292b7bbc446d5bc904343b4e7b5f1
unknown <yan01@.cmpa.org>
2010-04-08 12:19:10 -0400

    testing

---
+++ SchemaValidator/DatabaseSchema.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchemaValidator
+{
+    public enum DatabaseType
+    {
+        MSSQLServer=0,
+        Oracle=1
+    }
+
+    public class DatabaseSchema
+    {
+
+        // private fields
+        private string _connectionString;
+        private DatabaseType _dbType { get; private set; }
+
+
+        // constructors
+        public DatabaseSchema(DatabaseType DbType, string connectionString)
+        {
+            _dbType = DbType;
+            _connectionString = connectionString;
+        }
+
+        public DatabaseSchema(string connectionString)
+        {
+            _dbType = DatabaseType.MSSQLServer;
+            _connectionString = connectionString;
+        }
+
+
+        // properties
+
+
+    }
+}
