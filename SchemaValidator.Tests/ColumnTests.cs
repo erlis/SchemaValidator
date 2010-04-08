3366fec8884d026638a2d518b63b9f42dacd8f5d
Erlis <erlis@momentis.com>
2010-04-08 15:34:58 -0400

    CHANGED: ColumnTest.WithColumn return a column

--- SchemaValidator.Tests/ColumnTests.cs
+++ SchemaValidator.Tests/ColumnTests.cs
@@ -9,5 +9,25 @@ namespace SchemaValidator.Tests
     [TestFixture]
     public class ColumnTests
     {
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void WithColumn_should_return_class_Column()
+        {
+            // Arrange
+            Table _table = new Table("t1");
+            Column _column = _table.WithColumn("c1"); 
+
+            // Act 
+            Column actual = _column.WithColumn("c2"); 
+
+            // Assert
+            Assert.That(actual.Name, Is.EqualTo("c2")); 
+        }
     }
 }
