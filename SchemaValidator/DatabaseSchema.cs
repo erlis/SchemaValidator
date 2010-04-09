ed1b4bfd319ac894106a012a8ab885dfc1391059
Carlos Yanez <yanezpena@gmail.com>
2010-04-09 15:38:32 -0400

    Add IDatabaseSchema interface

--- SchemaValidator/DatabaseSchema.cs
+++ SchemaValidator/DatabaseSchema.cs
@@ -5,36 +5,31 @@ using System.Text;
 
 namespace SchemaValidator
 {
-    public enum DatabaseType
-    {
-        MSSQLServer=0,
-        Oracle=1
-    }
 
-    public class DatabaseSchema
+    public class DatabaseSchema : IDatabaseSchema
     {
 
         // private fields
         private string _connectionString;
-        private DatabaseType _dbType { get; set; }
 
 
         // constructors
-        public DatabaseSchema(DatabaseType DbType, string connectionString)
-        {
-            _dbType = DbType;
-            _connectionString = connectionString;
-        }
-
         public DatabaseSchema(string connectionString)
         {
-            _dbType = DatabaseType.MSSQLServer;
             _connectionString = connectionString;
         }
 
 
         // properties
+        public int LoadTables()
+        {
+            return 0;
+        }
 
+        public int LoadTableFields(string tableName)
+        {
+            return 0;
+        }
 
     }
 }
