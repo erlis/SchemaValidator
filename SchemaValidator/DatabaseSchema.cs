21b6c07db5082df566fbc4494b7b5f10d7fad59e
Carlos Yanez <yanezpena@gmail.com>
2010-04-09 16:21:26 -0400

    Add LoadTables method

--- SchemaValidator/DatabaseSchema.cs
+++ SchemaValidator/DatabaseSchema.cs
@@ -11,6 +11,7 @@ namespace SchemaValidator
 
         // private fields
         private string _connectionString;
+        private List<Table> _tableList;
 
 
         // constructors
@@ -21,14 +22,63 @@ namespace SchemaValidator
 
 
         // properties
+        public int TableCount { get { return _tableList.Count; } }
+
+        
+        // methods
         public int LoadTables()
+        {
+            System.Data.DataSet _records;
+
+            
+            return 0;
+        }
+
+        public int LoadTableFields(Table table)
         {
             return 0;
         }
 
-        public int LoadTableFields(string tableName)
+        private System.Data.DataSet LoadDBTables()
         {
-            return 0;
+            System.Data.DataSet _records = new System.Data.DataSet();
+            System.Data.SqlClient.SqlConnection _conn = null;
+            System.Data.SqlClient.SqlCommand _cmd;
+            System.Data.SqlClient.SqlDataAdapter _adapter;
+
+            try
+            {
+                // create connection
+                using (_conn = new System.Data.SqlClient.SqlConnection(this._connectionString))
+                {
+                    // open connection
+                    _conn.Open();
+
+                    // execute sql to get the tables
+                    using (_cmd = new System.Data.SqlClient.SqlCommand())
+                    {
+                        _cmd.CommandText = "";
+                        _cmd.Connection = _conn;
+
+                        using (_adapter = new System.Data.SqlClient.SqlDataAdapter())
+                        {
+                            _adapter.SelectCommand = _cmd;
+                            _adapter.Fill(_records, "");
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw(ex);
+            }
+            finally
+            {
+                if (_conn.State == System.Data.ConnectionState.Open) _conn.Close();
+            }
+
+            return _records;
         }
 
     }
