0c10d4f0fcc594d6fc78ecd7a1af3003fbc0e3ea
Carlos Yanez <yanezpena@gmail.com>
2010-04-10 09:48:05 -0400

    test

---
+++ SchemaValidator/IDatabaseSchema.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchemaValidator
+{
+    interface IDatabaseSchema
+    {
+        int LoadTables();
+        int LoadTableFields(Table table);
+    }
+}
