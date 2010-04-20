6624bc832f6f0e1a4239b4e3bbfd446ada514dbd
Erlis <erlis@momentis.com>
2010-04-20 11:50:39 -0400

    CHANGED: Recovering Deleted files

---
+++ SchemaValidator.Tests/Specification/DbSpecificationTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using System;
+using SchemaValidator.Specification;
+using SchemaValidator.ValueObjects.DBElements;
+
+// ReSharper disable InconsistentNaming
+namespace SchemaValidator.Tests.Specification
+{
+    [TestFixture]
+    public class DbSpecificationTests
+    {
+        [Test]
+        public void Constructor_should_initialize_by_receiving_table_list()
+        {
+            // Arrange
+            List<Table> tableList = new List<Table>
+                                        {
+                                            new Table("t1"),
+                                            new Table("t2"),
+                                        };
+
+            // Act
+            DbSpecification dbSpecification = new DbSpecification(tableList);
+
+            // Assert
+            Assert.That(dbSpecification.TableCount, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        [ExpectedException(ExpectedException = typeof(InvalidOperationException))]
+        public void AddTable_should_throw_exception()
+        {
+            // Arrange
+            SchemaSpecification spec = new DbSpecification(new List<Table>());
+
+            // Act
+            spec.AddTable("a table");
+        }
+    }
+}
---
+++ SchemaValidator/Specification/DbSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using SchemaValidator.ValueObjects.DBElements;
+namespace SchemaValidator.Specification
+{
+    public class DbSpecification : SchemaSpecification
+    {
+        /// constructor
+        public DbSpecification(List<Table> tableList)
+        {
+            tableList.ForEach( AddTable);
+        }
+
+        /// methods
+        public override Table AddTable(string tableName)
+        {
+            throw new InvalidOperationException("DB Specification cannot use AddTable");
+        }
+    }
+}
