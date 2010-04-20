6b70e8c54c574fa3813444e8ffbfb446db16ae61
Erlis <erlis@momentis.com>
2010-04-20 10:26:46 -0400

    ADDED: ManualSpecification.AssertIsSatisfiedBy which will throw exception if compare have value

--- SchemaValidator.Tests/Specification/ManualSpecificationTests.cs
+++ SchemaValidator.Tests/Specification/ManualSpecificationTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using SchemaValidator.Specification;
+using SchemaValidator.ValueObjects.DBElements;
+using System.Collections.Generic;
 
 // ReSharper disable InconsistentNaming
 namespace SchemaValidator.Tests.Specification
@@ -9,14 +11,38 @@ namespace SchemaValidator.Tests.Specification
     {
         [Test]
         [ExpectedException(ExpectedException = typeof(SpecificationException))]
-        [Ignore]
-        public void Validate_should_throw_exception_when_compare_has_values()
+        public void AssertIsSatisfiedBy_should_throw_exception_when_missing_tables()
         {
             // Arrange
+            List<Table> tableList = new List<Table>
+                                        {
+                                            new Table( "irrelevant")
+                                        };
+            DBSpecification dbSpecification = new DBSpecification(tableList);
             ManualSpecification manualSpecification = new ManualSpecification();
+            manualSpecification.AddTable("t1");
+
 
             // Act
-            manualSpecification.Validate(null /*DBSpecification*/); 
+            manualSpecification.AssertIsSatisfiedBy(dbSpecification); 
+        }
+
+        [Test]
+        [ExpectedException(ExpectedException = typeof(SpecificationException))]
+        public void AssertIsSatisfiedBy_should_throw_exception_when_conflict_tables()
+        {
+            // Arrange
+            List<Table> tableList = new List<Table>
+                                        {
+                                            new Table( "t1")
+                                        };
+            DBSpecification dbSpecification = new DBSpecification(tableList);
+            ManualSpecification manualSpecification = new ManualSpecification();
+            manualSpecification.AddTable("t1").WithColumn("c1");
+
+
+            // Act
+            manualSpecification.AssertIsSatisfiedBy(dbSpecification);
         }
     }
 }
