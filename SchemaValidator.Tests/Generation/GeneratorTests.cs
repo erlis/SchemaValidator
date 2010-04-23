14a1cf62681f38ed0ec29b745ff77d441da42918
Erlis <erlis@momentis.com>
2010-04-23 16:11:31 -0400

    CHANGED: Writing test result to disk

--- SchemaValidator.Tests/Generation/GeneratorTests.cs
+++ SchemaValidator.Tests/Generation/GeneratorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using NUnit.Framework;
 using SchemaValidator.Generation;
 
@@ -11,8 +12,11 @@ namespace SchemaValidator.Tests.Generation
         [Test]
         public void Test_Generation()
         {
-            TestGenerator generator = new TestGenerator("Data Source=mfstest;Initial Catalog=fosmaster;User ID=momentis;Password=HGDHSWGWYIBWGQDIJSGXDHZSGSRHXA;");
-            Console.Out.Write( generator.GenerateTest() );
+            TestGenerator generator = new TestGenerator("Data Source=mfstest;Initial Catalog=Rel5Test;User ID=momentis;Password=HGDHSWGWYIBWGQDIJSGXDHZSGSRHXA;");
+            StreamWriter streamWriter = File.CreateText(@"C:\temp\tests.cs");
+            streamWriter.Write( generator.GenerateTest() ); 
+            streamWriter.Flush();
+            streamWriter.Close();
         }
     }
 }
