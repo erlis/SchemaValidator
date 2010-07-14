917653477396c510bbe067b95177339a01a81a34
Erlis <erlis@momentis.com>
2010-07-14 13:20:13 -0400

    CHANGED: Using BDD style tests for SpecificationConflictTests

--- SchemaValidator.Tests/ValueObjects/Conflict/SpecificationConflictTests.cs
+++ SchemaValidator.Tests/ValueObjects/Conflict/SpecificationConflictTests.cs
@@ -1,29 +1,31 @@
 ﻿using NUnit.Framework;
-using SchemaValidator.ValueObjects;
 using SchemaValidator.ValueObjects.Conflict;
 using SchemaValidator.ValueObjects.DBElements;
 
 // ReSharper disable InconsistentNaming
 namespace SchemaValidator.Tests.ValueObjects.Conflict
 {
-    [TestFixture]
     public class SpecificationConflictTests
     {
 
-        [Test]
-        public void Constructor_should_receive_a_TablePair_with_Detail()
+        [TestFixture]
+        public class WhenCreatingAnSpecificationConflict
         {
-            // Arrange
-            var pair = new Pair<Table>(new Table("irrelevant"), new Table("irrelevant"));
-            var compareResult = new CompareResult ();
+            [Test]
+            public void GivenATablePairAndACompareResult_ThenFirstAndSecondAndDetailAreDelegatesOfThem()
+            {
+                // Arrange
+                var pair = new Pair<Table>(new Table("irrelevant"), new Table("irrelevant"));
+                var compareResult = new CompareResult();
 
-            // Act
-            var conflict = new SpecificationConflict( pair, compareResult);
+                // Act
+                var conflict = new SpecificationConflict(pair, compareResult);
 
-            // Assert
-            Assert.That( conflict.Detail, Is.SameAs(compareResult));
-            Assert.That(conflict.First, Is.EqualTo(pair.First));
-            Assert.That(conflict.Second, Is.EqualTo(pair.Second));
+                // Assert
+                Assert.That(conflict.Detail, Is.SameAs(compareResult));
+                Assert.That(conflict.First, Is.EqualTo(pair.First));
+                Assert.That(conflict.Second, Is.EqualTo(pair.Second));
+            }
         }
 
     }
