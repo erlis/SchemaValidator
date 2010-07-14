ee2536cdf71f4a128dcbbb45d8f85f9c4e90876c
Erlis <erlis@momentis.com>
2010-07-14 15:44:10 -0400

    CHANGED: Using BDD test style for TableTests

--- SchemaValidator.Tests/ValueObjects/DBElement/TableTests.cs
+++ SchemaValidator.Tests/ValueObjects/DBElement/TableTests.cs
@@ -6,184 +6,198 @@ using SchemaValidator.ValueObjects.DBElements;
 // ReSharper disable InconsistentNaming
 namespace SchemaValidator.Tests.ValueObjects.DBElement
 {
-    [TestFixture]
     public class TableTests
     {
-        [Test]
-        public void Compare_should_allow_same_tableNames()
+        [TestFixture]
+        public class When_creating
         {
-            // Arrange
-            Table t1 = new Table("t1");
-            Table t2 = new Table("t1");
-
-            // Act
-            CompareResult result = t1.Compare(t2);
-
-            // Assert
-            Assert.That(result, Is.Not.Null);
+            [ExpectedException(ExpectedException = typeof(ArgumentException))]
+            [Test] public void Given_null_as_table_name_then_should_fail()
+            {
+                // Assert
+                new Table(null);
+            }
         }
 
-        [Test]
-        public void Compare_should_be_case_insensitive()
+        [TestFixture]
+        public class When_comparing
         {
-            // Arrange
-            Table t1 = new Table("T1");
-            Table t2 = new Table("t1");
+            [Test] public void Should_have_same_table_names_to_be_a_valid_operation()
+            {
+                // Arrange
+                Table t1 = new Table("t1");
+                Table t2 = new Table("t1");
 
-            // Act
-            CompareResult result = t1.Compare(t2);
+                // Act
+                CompareResult result = t1.Compare(t2);
 
-            // Assert
-            Assert.That(result, Is.Not.Null);
+                // Assert
+                Assert.That(result, Is.Not.Null);
+            }
+
+            [ExpectedException(ExpectedException = typeof(InvalidOperationException))]
+            [Test] public void Given_a_table_with_different_name_then_should_throw_InvalidOperationException()
+            {
+                // Arrange
+                Table t1 = new Table("t1");
+                Table t2 = new Table("t2");
+
+                // Act
+                t1.Compare(t2);
+            }
+
+            [Test] public void Then_table_names_are_case_insensitive()
+            {
+                // Arrange
+                Table t1 = new Table("T1");
+                Table t2 = new Table("t1");
+
+                // Act
+                CompareResult result = t1.Compare(t2);
+
+                // Assert
+                Assert.That(result, Is.Not.Null);
+            }
+
+            [Test] public void Given_a_table_with_different_columnType_then_conflict_should_be_detected()
+            {
+                // Arrange
+                Table t1 = new Table("t1");
+                Column expectedFirst = t1.WithColumn("c1").OfType("int", 4);
+                t1.WithColumn("irrelevant").OfType("varchar", 3);
+
+                Table t2 = new Table("t1");
+                Column expectedSecond = t2.WithColumn("c1").OfType("varchar", 4);
+                t2.WithColumn("irrelevant").OfType("varchar", 3);
+
+                // Act 
+                CompareResult result = t1.Compare(t2);
+
+                // Assert
+                Assert.That(result.ConflictList.Count, Is.EqualTo(1));
+                Assert.That(result.ConflictList[0].First, Is.SameAs(expectedFirst));
+                Assert.That(result.ConflictList[0].Second, Is.SameAs(expectedSecond));
+            }
+
+            [Test] public void Given_a_table_with_a_missing_column_then_the_missing_column_should_be_detected()
+            {
+                // Arrange
+                Table t1 = new Table("t1");
+                t1.WithColumn("c1");
+
+                Table t2 = new Table("t1");
+
+                // Act 
+                CompareResult result = t1.Compare(t2);
+
+                // Assert
+                Assert.That(result.MissingList.Count, Is.EqualTo(1));
+                Assert.That(result.MissingList[0].Name, Is.EqualTo("c1"));
+            }
+            
+            [Test] public void Given_a_table_with_extra_columns_then_the_extra_columns_should_be_ignored()
+            {
+                // Arrange
+                Table t1 = new Table("t1");
+                t1.WithColumn("c1");
+
+                Table t2 = new Table("t1");
+                t2.WithColumn("c1");
+                t2.WithColumn("c2");
+
+                // Act
+                CompareResult result = t1.Compare(t2);
+
+                // Assert 
+                Assert.That(result.HaveValues, Is.False);
+            }
         }
 
-        [Test]
-        public void Compare_should_detect_different_columns()
+        [TestFixture]
+        public class Given_Table_should_be_fluent
         {
-            // Arrange
-            Table t1 = new Table("t1");
-            Column expectedFirst = t1.WithColumn("c1").OfType("int", 4);
-            t1.WithColumn("irrelevant").OfType("varchar", 3);
+            [Test] public void Then_WithColumn_should_be_used_to_add_columns_to_a_table()
+            {
+                // Arrange
+                Table table = new Table("table1");
 
-            Table t2 = new Table("t1");
-            Column expectedSecond = t2.WithColumn("c1").OfType("varchar", 4);
-            t2.WithColumn("irrelevant").OfType("varchar", 3);
+                // Act 
+                table.WithColumn("c1")
+                     .WithColumn("c2")
+                     .WithColumn("c3");
 
-            // Act 
-            CompareResult result = t1.Compare(t2);
+                // Assert
+                Assert.That(table.Columns.Count, Is.EqualTo(3));
+            }
 
-            // Assert
-            Assert.That(result.ConflictList.Count, Is.EqualTo(1));
-            Assert.That(result.ConflictList[0].First, Is.SameAs(expectedFirst));
-			Assert.That( result.ConflictList[ 0 ].Second, Is.SameAs( expectedSecond ) );
-		}
+            [Test] public void Then_WithColumn_should_create_a_new_column()
+            {
+                // Arrange
+                Table table = new Table("table1");
 
-        [Test]
-        public void Compare_should_detect_missing_columns()
-        {
-            // Arrange
-            Table t1 = new Table("t1");
-            t1.WithColumn("c1");
+                // Act
+                Column column = table.WithColumn("Column1");
 
-            Table t2 = new Table("t1");
+                // Assert
+                Assert.That(column.Name, Is.EqualTo("Column1"));
+            }
 
-            // Act 
-            CompareResult result = t1.Compare(t2);
+            [ExpectedException(ExpectedException = typeof(ApplicationException))]
+            [Test] public void When_twice_the_same_column_name_then_WithColumn_should_throw_ApplicationException()
+            {
+                // Arrange
+                Table table = new Table("table1");
 
-            // Assert
-            Assert.That(result.MissingList.Count, Is.EqualTo(1));
-            Assert.That(result.MissingList[0].Name, Is.EqualTo("c1"));
+                // Act
+                table.WithColumn("c1")
+                     .WithColumn("c1");
+            }
+
+            [ExpectedException(ExpectedException = typeof(ApplicationException))]
+            [Test] public void When_twice_the_same_column_name_with_different_case_then_WithColumn_should_throw_ApplicationException()
+            {
+                // Arrange
+                Table table = new Table("table1");
+
+                // Act
+                table.WithColumn("c1")
+                     .WithColumn("C1");
+            }
+
+            [Test] public void Then_Done_will_end_the_fluent_sequence_for_columns()
+            {
+                // Arrange
+                Table table = new Table("Table1")
+                    .WithColumn("column1")
+                    .Done(); 
+
+                // Assert
+                Assert.That( table, Is.InstanceOf(typeof(Table)));
+            }
         }
 
-        [Test]
-        public void Compare_should_ignore_extra_columns_in_other_table()
+        [TestFixture]
+        public class When_representing_as_string
         {
-            // Arrange
-            Table t1 = new Table("t1");
-            t1.WithColumn("c1");
+            [Test] public void Given_a_table_with_the_columns_then_ToString_should_return_string_representation()
+            {
+                // Arrange
+                Table table = new Table("Person")
+                    .WithColumn("Id").OfType("int", 4)
+                    .WithColumn("Name").OfType("varchar", 255).Nullable()
+                    .WithColumn("Salary").OfType("double", 2)
+                    .Done();
+                string expected = "[Person]\n" +
+                                  "   Id : int(4)\n" +
+                                  "   Name : varchar(255) NULLABLE\n" +
+                                  "   Salary : double(2)\n";
 
-            Table t2 = new Table("t1");
-            t2.WithColumn("c1");
-            t2.WithColumn("c2");
+                // Act
+                string result = table.ToString();
 
-            // Act
-            CompareResult result = t1.Compare(t2);
-
-            // Assert 
-            Assert.That(result.HaveValues, Is.False);
-        }
-
-        [Test]
-        [ExpectedException(ExpectedException = typeof(InvalidOperationException))]
-        public void Compare_should_throw_exception_when_different_tableNames()
-        {
-            // Arrange
-            Table t1 = new Table("t1");
-            Table t2 = new Table("t2");
-
-            // Act
-            t1.Compare(t2);
-        }
-
-        [Test]
-        [ExpectedException(ExpectedException = typeof(ArgumentException))]
-        public void Constructor_should_fail_when_tableName_null()
-        {
-            // Assert
-            new Table(null);
-        }
-
-        [Test]
-        public void ToString_should_return_string_representation()
-        {
-            // Arrange
-            Table table = new Table("Person").WithColumn("Id").OfType("int", 4)
-                                             .WithColumn("Name").OfType("varchar", 255).Nullable()
-                                             .WithColumn("Salary").OfType("double", 2)
-                                             .Done();
-            string expected = "[Person]\n" +
-                              "   Id : int(4)\n" +
-                              "   Name : varchar(255) NULLABLE\n" +
-                              "   Salary : double(2)\n";
-
-            // Act
-            string result = table.ToString(); 
-
-            // Assert
-            Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        [ExpectedException(ExpectedException = typeof(ApplicationException))]
-        public void WithColumn_should_be_case_insensitive()
-        {
-            // Arrange
-            Table table = new Table("table1");
-
-            // Act
-            table.WithColumn("c1")
-                 .WithColumn("C1");
-        }
-
-        [Test]
-        public void WithColumn_should_be_concatenated_fluently()
-        {
-            // Arrange
-            Table table = new Table("table1");
-
-            // Act 
-            table.WithColumn("c1")
-                 .WithColumn("c2")
-                 .WithColumn("c3");
-
-            // Assert
-            Assert.That(table.Columns.Count, Is.EqualTo(3));
-        }
-
-        [Test]
-        public void WithColumn_should_return_same_class_Table()
-        {
-            // Arrange
-            Table table = new Table("table1");
-
-            // Act
-            Column column = table.WithColumn("Column1");
-
-            // Assert
-            Assert.That(column.Name, Is.EqualTo("Column1"));
-        }
-
-
-        [Test]
-        [ExpectedException(ExpectedException = typeof(ApplicationException))]
-        public void WithColumn_should_throw_exception_when_duplicated()
-        {
-            // Arrange
-            Table table = new Table("table1");
-
-            // Act
-            table.WithColumn("c1")
-                 .WithColumn("c1");
+                // Assert
+                Assert.That(result, Is.EqualTo(expected));
+            }
         }
     }
 }
